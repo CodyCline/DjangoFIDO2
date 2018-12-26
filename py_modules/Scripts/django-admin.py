9baaaf316caf1e674e3ef7e83f58e5682112781b
CodyCline <clinecody98@gmail.com>
2018-12-26 13:52:56 -0800

    Initial commit; added model for keys and users

--- py_modules/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\cline\desktop\projects\djangofido2\py_modules\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
