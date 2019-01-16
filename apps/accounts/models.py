d43f2696cd3247e1059b97482947932ddf7cd51b
CodyCline <clinecody98@gmail.com>
2019-01-16 13:33:17 -0800

    Authentication from db now working for multiple authenticators

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,9 +1,12 @@
 import datetime
+
 from django.shortcuts import HttpResponse
 from django.contrib.auth.models import AbstractUser, UserManager
 from django.db import models
 from django.conf import settings
-from fido2 import cbor
+
+from fido2.ctap2 import AttestedCredentialData
+from fido2.utils import websafe_encode, websafe_decode
 
 class ModelManager(models.Manager): 
 	#This may end up being a queryset
@@ -20,29 +23,30 @@ class ModelManager(models.Manager):
 		return new_authenticator.id
 	
 	def get_credentials(self, user):
-		#This needs to grab the credentials that belong to a user based on username
+		#This function does validation and if it passes all the checks 
+		#it then returns a list of all of the user's authenticator data
 		query_list = []
-		user_id = CustomUser.objects.get(username=user).id
-		#TODO return statement if username not found
-		if user_id.exists() is None:
-			return HttpResponse("We can't find that user")
-		#Also need to use exists() method to check if they have any keys
-		auth_query = Authenticator.objects.filter(user=user_id).all()
-		if auth_query.exists():
-			for i in auth_query:
-				query_list.append(Authenticator.objects.only('encoded_data').get(user=user_id).encoded_data)
-			return query_list
-		else:
-			return HttpResponse('Not found')
-		
-		#Grab each AttestedCredentialData that belongs to an Authenticator
-		# whose user_id matches up with the logged in user' id
-		#This is where we will probably need to reconstruct the data
-		#to be in a proper format
+		username = CustomUser.objects.get(username=user).id
+		# print('This is the user id \n', user_id, '\n type \n', type(user_id))
+		#Check if the user they inputted exists
+		# if CustomUser.objects.filter(id=user_id).exists():
+			# return HttpResponse("We can't find that user")
+		#Goes to db and grabs the value of the data stored from device 
+		# and then returns a list of said data
+		auth_query = Authenticator.objects.filter(user=username).all()
+		# if auth_query.exists():
+		for cred in auth_query.iterator():
+			query = cred.encoded_data
+			decode = AttestedCredentialData(websafe_decode(query))
+			query_list.append(decode)
 		return query_list
+		# else:
+			# print('Not found')
+			# return HttpResponse("We can't find any devices registered to this user")
+		
 	
 	def get_password_by_username(self, username):
-		the_user = CustomUser.objects.get(username=user).password
+		the_user = CustomUser.objects.get(username=username).password
 		return the_user
 
 	#This will update the 'last_used' property every 
@@ -62,7 +66,7 @@ class CustomUser(AbstractUser):
 class Authenticator(models.Model):
 	user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="keys", on_delete=models.CASCADE)
 	name = models.CharField(max_length=50)
-	encoded_data = models.CharField(max_length=950)
+	encoded_data = models.CharField(max_length=950) #Data stored from key
 	last_used = models.DateTimeField(auto_now_add=True)
 	created_at = models.DateTimeField(auto_now_add=True)
 	objects = ModelManager()
--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,6 +1,7 @@
 import json
 
 from django.shortcuts import render, redirect, HttpResponse
+from django.contrib.auth import authenticate, login
 from django.views.decorators.http import require_http_methods
 from django.http import Http404
 
@@ -9,17 +10,17 @@ from .forms import CustomUserCreationForm
 
 
 from fido2.client import ClientData
-from fido2.server import U2FFido2Server, RelyingParty
+from fido2.server import U2FFido2Server, RelyingParty, Fido2Server
 from fido2.ctap2 import AttestationObject, AuthenticatorData
 from fido2.ctap2 import AttestedCredentialData as att_data
 from fido2.ctap1 import RegistrationData
 from fido2.utils import sha256, websafe_encode, websafe_decode
 from fido2 import cbor
 
-rp = RelyingParty('localhost', 'Demo FIDO2 server')
-server = U2FFido2Server('https://localhost:9000', rp)
+# rp_host = urlparse(request.build_absolute_uri()).hostname
+rp = RelyingParty('localhost', 'Demo server')
+server = Fido2Server(rp)
 
-# credentials = []
 
 def user_create(request, template_name='accounts/signup.html'):
 	form = CustomUserCreationForm(request.POST)
@@ -34,14 +35,14 @@ def user_create(request, template_name='accounts/signup.html'):
 def start_registration(request):
 	#TODO: Need to check if authenticator exists already within db
 	#Creates a challenge with user_id and email and returns as CBOR data
-
-	Authenticator.objects.get_credentials(user=request.user.username)
+	print(request.user.username)
+	credentials = Authenticator.objects.get_credentials(user=request.user.username)
 
 	registration_data, state = server.register_begin({
 		'id': bytes(request.user.id),
 		'name': request.user.email,
 		'displayName': request.user.username
-	}, query)
+	}, credentials)
 
 	request.session['state'] = state
 	return HttpResponse(cbor.dumps(registration_data))
@@ -73,14 +74,7 @@ def start_authentication(request):
 	data = json.loads(body_unicode)
 	
 	#Lookup current keys
-	credentials = []
-
-	encoded = Authenticator.objects.only('encoded_data').get(pk=1).encoded_data
-	safe = att_data(websafe_decode(encoded))
-
-	credentials.append(safe)
-
-	print('These are the credentials', credentials, '\n\n This is the type \n\n', type(credentials))
+	credentials = Authenticator.objects.get_credentials(user=data['username'])
 
 	auth_data, state = server.authenticate_begin(credentials)
 	request.session['state'] = state
@@ -96,14 +90,9 @@ def finish_key(request):
 	signature = data['signature']	
 
 	#Grab the credentials again from db use session to look up user
-	#However, for some reason, this needs to be in its natural format ?
-	# credentials = Authenticator.objects.get_credentials(user=request.session['username'])
-	credentials = []
-	decode_this = Authenticator.objects.only('encoded_data').get(pk=1).encoded_data
-	# creds = att_data(websafe_decode(query)
-	creds = att_data(websafe_decode(decode_this))
-	credentials.append(creds)
-
+	
+	credentials = Authenticator.objects.get_credentials(user=request.session['username'])
+	
 	server.authenticate_complete(
 		request.session.pop('state'),
 		credentials,
@@ -112,16 +101,15 @@ def finish_key(request):
 		auth_data,
 		signature
 	)
+	
+	user_logging_in = request.session['username'] # This needs to be session data
+	password = CustomUser.methods.get_password_by_username(username=user_logging_in)
+	user = authenticate(request, user=user_logging_in, password=password)
+	if user is not None:
+		login(request, user)
+	return redirect('/')	
 
-	# if auth_finish:
-	# 	user_logging_in = request.session['username'] # This needs to be session data
-	# 	password = CustomUser.objects.get_password_by_username(user=user_logging_in)
-	# 	user = authenticate(request, username=user_logging_in, password=password)
-	# 	if user is not None:
-	# 		login(request, user)
-	# 	return HttpResponse("This checks out 👍")		
-
-	return HttpResponse(cbor.dumps({'status': 'OK'}))
+	# return HttpResponse(cbor.dumps({'status': 'OK'}))
 
 #Views for editing or deleting keys. These will likely be ajax views
 def edit_authenticator(request):
